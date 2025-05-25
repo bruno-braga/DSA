42d95519aebe07a66791a236b4b31300ef54a938
Bruno <brunobraga@protonmail.com>
2025-05-25 18:54:43 -0300

    createList and add another node to it

--- linkedlist.zig
+++ linkedlist.zig
@@ -1,9 +1,25 @@
+const std = @import("std");
+
 const Node = struct {
     data: ?i32= null,
     next: ?*Node = null,
 };
 
+pub fn createList(n: ?i32) Node {
+    var node = Node{};
+    node.data = n;
+    node.next = null;
+
+    return node;
+}
 
 pub fn main() void {
+    var list = createList(1);
+    
+    var next_node = Node{};
+    list.next = &next_node;
 
+    list.next.?.data = 2;
+
+    std.debug.print("List: {any}\n", .{list});
 }
