bd16f994693b80b49dc7eb7bd832ffce1611ef6f
Bruno <brunobraga@protonmail.com>
2025-05-29 14:24:22 -0300

    Finish AddAtEnd

--- linkedlist.zig
+++ linkedlist.zig
@@ -1,4 +1,7 @@
 const std = @import("std");
+const heap = std.heap;
+const print = std.debug.print;
+const AllocatorType = std.mem.Allocator;
 
 const Node = struct {
     data: ?i32= null,
@@ -22,24 +25,43 @@ fn traverse(list: *Node) void {
     }
 }
 
-fn addAtEnd(list: *Node, n: ?i32) void {
-    var next_node = Node{};
-    list.next = &next_node;
+fn addAtEnd(list: *Node, allocator: AllocatorType, n: ?i32) !Node {
+    var temp: ?*Node = list;
+    while(temp.?.next != null) {
+        temp = temp.?.next;
+    }
 
-    list.next.?.data = n;
+    const node_ptr = try allocator.create(Node);
+    errdefer allocator.destroy(node_ptr); // avoid memory leak in case of a fail
+
+    node_ptr.* = Node{
+        .data = n,
+        .next = null,
+    };
+
+    temp.?.next = node_ptr;
+
+    return node_ptr.*;
 }
 
 pub fn main() void {
+    var gpa = heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
     var list = createList(1);
-    
-    // var next_node = Node{};
-    // list.next = &next_node;
 
-    // list.next.?.data = 2;
-
-    addAtEnd(&list, 2);
+    _ = addAtEnd(&list, allocator, 2) catch @panic("Failed to add node");
+    _ = addAtEnd(&list, allocator, 3) catch @panic("Failed to add node");
 
     traverse(&list);
 
-    std.debug.print("List: {any}\n", .{list});
+    print("List: {any}\n", .{list});
+
+    var current: ?*Node = list.next;
+    while (current != null) {
+        const next = current.?.next;
+        allocator.destroy(current.?);
+        current = next;
+    }
 }
